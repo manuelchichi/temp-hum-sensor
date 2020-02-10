7cec42679b8eec7ec86332820103db3daa7ec952
Manuel Chichi <manuchichi@hotmail.com>
2020-02-10 17:22:09 -0300

    Add files via upload

--- master.js
+++ master.js
@@ -3,6 +3,15 @@ var fs = require('fs')
 var app = express()
 var bodyParser = require('body-parser');
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,7 +20,7 @@ app.get('/', function(request, response) {
   var html = `
     <html>
         <body>
-            <form method="post" action="http://localhost:3000">Name: 
+            <form method="post" action="https://temp-hum-sensor.herokuapp.com">Name: 
                 <input type="text" name="name" />
                 <input type="submit" value="Submit" />
             </form>
@@ -24,6 +33,18 @@ app.get('/', function(request, response) {
 app.post('/', function(request, response) {
   console.log('POST /')
   console.dir(request.body)
+
+  const text = 'INSERT INTO lectures(temperature, humidity,time) VALUES($1, $2, $3) RETURNING *'
+  const values = [request.body.temperature, request.body.humidity,Date.now()]
+  // callback
+  client.query(text, values, (err, res) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log(res.rows[0])
+    }
+  })
+
   response.writeHead(200, {'Content-Type': 'text/html'})
   response.end('thanks')
 })
